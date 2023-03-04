fdb8fa2210ef3627b829b4cdaa3e00b2bef56f15
osamaamerr97 <122517339+osamaamerr97@users.noreply.github.com>
2023-03-04 20:59:46 +0300

    first navbar

---
+++ components/TheHeader.vue
@@ -0,0 +1,51 @@
+<template lang="">
+    <div>
+      <div class="bg-main pt-2">
+    <b-navbar toggleable="lg" class='container'>
+      <b-navbar-brand href="#" >NavBar</b-navbar-brand>
+
+      
+      <b-navbar-toggle target="nav-collapse" toggleable @click='isNavOpen = !isNavOpen'>
+        <font-awesome-icon  v-if='isNavOpen == true'  :icon="['fas', 'xmark']"  class='close-icon'/>
+      </b-navbar-toggle>
+
+
+
+      <b-collapse id="nav-collapse" is-nav>
+          
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-navbar-nav>
+            <b-nav-item href="#" class='active'>Home</b-nav-item>
+            <b-nav-item href="#">Solutions</b-nav-item>
+            <b-nav-item href="#">Industries</b-nav-item>
+            <b-nav-item href="#">Portfolio</b-nav-item>
+            <b-nav-item href="#">About Us</b-nav-item>
+          </b-navbar-nav>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+        </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isNavOpen:false
+    }
+  },
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <TheHeader />
+        <nuxt-child />
+      <TheFooter />
+    </div>
+  </template>
+  <script>
+  export default {
+    name: 'IndexPage'
+  }
+  </script>
+  
