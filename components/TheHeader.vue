bd2d420f206580bc5ab2cd8aa1a7adc7c97e70ff
osamaamerr97 <122517339+osamaamerr97@users.noreply.github.com>
2023-03-04 23:13:38 +0300

    edit navbar

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,51 +1,233 @@
 <template lang="">
-    <div>
-      <div class="bg-main pt-2">
-    <b-navbar toggleable="lg" class='container'>
-      <b-navbar-brand href="#" >NavBar</b-navbar-brand>
+    <div class="nav-container bg-main">
+      <div class="container">
+        <nav>
+            <div class="logo">
+                <!-- <img src="logo.svg" alt="Logo Image"> -->
+                <h2>Namaa</h2>
+            </div>
+            <div class="hamburger "  @click="toggleMenu" :class="{'toggle': isMenuOpen ==true}">
+                <div class="line1"></div>
+                <div class="line2"></div>
+                <div class="line3"></div>
+            </div>
+            <ul class="nav-links" :class="{'open': isMenuOpen ==true}">
+              <li class="nav-link"  :class="{'fade': isMenuOpen ==true}">
+                <nuxtLink to="#" class='active'>Home</nuxtLink>
 
-      
-      <b-navbar-toggle target="nav-collapse" toggleable @click='isNavOpen = !isNavOpen'>
-        <font-awesome-icon  v-if='isNavOpen == true'  :icon="['fas', 'xmark']"  class='close-icon'/>
-      </b-navbar-toggle>
+              </li>
+              <li class="nav-link" :class="{'fade': isMenuOpen ==true}">
+                <nuxtLink to="#">Solutions</nuxtLink>
 
+              </li>
+              
+              <li class="nav-link" :class="{'fade': isMenuOpen ==true}">
+                <nuxtLink to="#">Industries</nuxtLink>
 
+              </li>
+              <li class="nav-link" :class="{'fade': isMenuOpen ==true}">
+                <nuxtLink to="#">Portfolio</nuxtLink>
 
-      <b-collapse id="nav-collapse" is-nav>
-          
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-navbar-nav>
-            <b-nav-item href="#" class='active'>Home</b-nav-item>
-            <b-nav-item href="#">Solutions</b-nav-item>
-            <b-nav-item href="#">Industries</b-nav-item>
-            <b-nav-item href="#">Portfolio</b-nav-item>
-            <b-nav-item href="#">About Us</b-nav-item>
-          </b-navbar-nav>
+              </li>
+              <li class="nav-link" :class="{'fade': isMenuOpen ==true}">
+                <nuxtLink to="#">About Us</nuxtLink>
 
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-        </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
-</div>
+              </li>
+            </ul>
+        </nav>
+      </div>
     </div>
 </template>
+
+
+
 <script>
 export default {
   data() {
     return {
-      isNavOpen:false
+      isMenuOpen: false,
     }
+  },
+  methods: {
+    toggleMenu() {
+
+      //Hamburger Animation
+      this.isMenuOpen = !this.isMenuOpen;
+    },
   },
     
 }
 </script>
-<style lang="">
-    
+<style scoped>
+.nav-container{
+  position: fixed;
+
+  top: 0;
+    left: 0;
+    right: 0;
+    z-index: 99;
+}
+nav{
+    padding-top: 30px;
+    padding-bottom: 1px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    color: #FFF;
+
+}
+
+/*Styling Links*/
+.nav-links{   /* ul */
+    display: flex;
+    justify-content: space-around;
+    list-style: none; 
+    align-items: center;
+    margin-left: auto;
+    width: 50%;
+    margin-bottom: 0;
+
+}
+.nav-links li {
+    position: relative;
+    padding-bottom: 15px;
+}
+.nav-links li a{
+    text-decoration: none;
+    color: #DDD;
+}
+
+
+.nav-link::before{
+    content: '';
+    width: 0%;
+    height: 2px;
+    background-color: #c38ea0;
+    position: absolute;
+    left:  5px;
+    bottom: 0;
+    transition:width 3s;
+    z-index: 999;
+
+}
+.nav-links li:hover a,
+.nav-link .active {
+    color: #FFF !important;
+}
+.nav-link:hover::before{
+    content: '';
+    width: 90%;
+    height: 2px;
+    background-color: #c38ea0;
+    position: absolute;
+    left:  5px;
+    bottom: 0;
+}
+
+
+.nav-link .active::before{
+    content: '';
+    width: 90%;
+    height: 2px;
+    background-color: #c38ea0;
+    position: absolute;
+    left:  5px;
+    bottom: 0;
+}
+
+
+
+/*Styling Hamburger Icon*/
+.hamburger div{
+    width: 30px;
+    height:3px;
+    background: #f2f5f7;
+    margin: 5px;
+    transition: all 0.3s ease;
+}
+.hamburger{
+    display: none;
+}
+
+/*Stying for small screens*/
+@media screen and (max-width: 800px){
+    nav{
+        position: relative;
+        z-index: 999;
+    }
+    .hamburger{
+        display:block;
+        position: absolute;
+        cursor: pointer;
+        right: 5%;
+        top: 50%;
+        transform: translate(-5%, -50%);
+        z-index: 2;
+        transition: all 0.7s ease;
+    }
+    .nav-links{
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        justify-content: start;
+
+        background: #666565;
+        /* opacity:.5; */
+        height: 100vh;
+        width: 100%;
+        flex-direction: column;
+        clip-path: circle(50px at 90% -20%);
+        -webkit-clip-path: circle(50px at 90% -10%);
+        transition: all 1s ease-out;
+        pointer-events: none;
+        padding: 0;
+        margin: 0;
+    }
+    .nav-links.open{
+        clip-path: circle(1000px at 90% -10%);
+        -webkit-clip-path: circle(1000px at 90% -10%);
+        pointer-events: all;
+    }
+    .nav-links li{
+        opacity: 0;
+        margin-top: 60px;
+    }
+    .nav-links li:nth-child(1){
+        transition: all 0.5s ease 0.2s;
+    }
+    .nav-links li:nth-child(2){
+        transition: all 0.5s ease 0.4s;
+    }
+    .nav-links li:nth-child(3){
+        transition: all 0.5s ease 0.6s;
+    }
+    .nav-links li:nth-child(4){
+        transition: all 0.5s ease 0.7s;
+    }
+    .nav-links li:nth-child(5){
+        transition: all 0.5s ease 0.8s;
+    }
+    .nav-links li:nth-child(6){
+        transition: all 0.5s ease 0.9s;
+        margin: 0;
+    }
+    .nav-links li:nth-child(7){
+        transition: all 0.5s ease 1s;
+        margin: 0;
+    }
+    li.fade{
+        opacity: 1;
+    }
+}
+/*Animating Hamburger Icon on Click*/
+.toggle .line1{
+    transform: rotate(-45deg) translate(-5px,6px);
+}
+.toggle .line2{
+    transition: all 0.7s ease;
+    width:0;
+}
+.toggle .line3{
+    transform: rotate(45deg) translate(-5px,-6px);
+}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,74 +7,34 @@
       :interval="4000"
       controls
       indicators 
-      background="#ababab"
+      background="#6C1835"
       img-width="1024"
-      img-height="480"
+      img-height="100"
       style="text-shadow: 1px 1px 2px #333;"
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
-      <!-- Text slides with image -->
-      <!-- <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"
-      ></b-carousel-slide> -->
-
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58">
+      <b-carousel-slide class="h-100 ">
         <div>
-          <h1>Hello world!</h1>
-          <h1>Hello world!</h1>
-          <h1>Hello world!</h1>
-          <h1>Hello world!</h1>
+          <h1>Namaa <br/> Leading Mobile App <br/> Development Company</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde tenetur maiores quam corporis</p>
+          <button>Vieew portofolio</button>
 
         </div>
+        
+      </b-carousel-slide>
+      <b-carousel-slide class="h-100 ">
+        <div>
+          <h1>yhgtfrhgf <br/> Leading Mobile App <br/> Development Company</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde tenetur maiores quam corporis</p>
+          <button>Vieew portofolio</button>
+
+        </div>
+        
       </b-carousel-slide>
 
-      <!-- Slides with image only -->
-      <!-- <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide> -->
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <!-- <b-carousel-slide>
-        <template #img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot"
-          >
-        </template>
-      </b-carousel-slide> -->
-
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <!-- <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        </p>
-      </b-carousel-slide> -->
     </b-carousel>
-
-    <!-- <p class="mt-4">
-      Slide #: {{ slide }}<br>
-      Sliding: {{ sliding }}
-    </p> -->
   </div>
-
-
-
-
-
-
-<!--   
-  <div class="home-header">
-      <h1>Namaa Leading Mobile App Development Company</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia aspernatur cum sit unde laboriosam nobis soluta fuga.</p>
-      <button>View Portfolio</button>
-  </div> -->
 </template>
 <script>
 export default {
